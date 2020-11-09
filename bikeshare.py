81569a107166a15c9e45afcbc23445dc91561e50
alngkxm17 <kmclean@atlanticlng.com>
2020-11-09 08:16:05 -0400

    refactor: Improvement of readability of bikeshare python code

--- bikeshare.py
+++ bikeshare.py
@@ -241,7 +241,7 @@ def main():
         user_stats(df)
 
         #comment to change in git
-        #this line improve efficiency
+        #In fact, this line improves readability
         raw_data(df)
 
         restart = input('\nWould you like to restart? Enter yes or no.\n')
